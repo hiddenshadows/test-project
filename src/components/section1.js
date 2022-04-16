a55ea895393361637c531996c6ca477be989191b
Davin Muramoto <davinmuramoto@gmail.com>
2022-04-15 20:01:04 -0700

    home

--- src/components/section1.js
+++ src/components/section1.js
@@ -1,85 +1,15 @@
 import React, {useState} from 'react';
-import './section1index.css'
+
 
 
 function Section1() {
 
-    const [weight, setWeight] = useState(0)
-  const [height, setHeight] = useState(0)
-  const [bmi, setBmi] = useState('')
-  const [message, setMessage] = useState('')
-
-  let imgSrc;
-
-  
-
-  let calcBmi  = (event) =>{
-    //prevetn submitting
-    event.preventDefault()
-
-    if(weight === 0 || height === 0){
-      alert('Please enter valid values')
-    }else{
-      const bmi = (weight / (height*height) *703)
-      setBmi(bmi.toFixed(1))
-
-      if(bmi<25){
-        setMessage('You are underweight')
-      }else if (bmi >= 25 && bmi < 30){
-        setMessage('You are a healthy weight')
-      }else{
-        setMessage('You are overweight')
-      }
-    }
-  }
-
-  if(bmi<1){
-    imgSrc = null
-  }else{
-    if(bmi<25){
-      imgSrc = require('../assets/skinny.jpg')
-    }else if(bmi >= 25 && bmi < 30){
-      imgSrc = require('../assets/normal.jpg')
-    }else{
-      imgSrc = require('../assets/fat.jpg')
-    }
-  } 
-
-
-  let reload = () =>{
-    window.location.reload()
-  }
-
-
+    let home = require('../assets/home.jpg');
 
     return (
-        <div className="app12">
-      <div className = 'container12'>
-        <h2 className = 'center12'BMI Calculator></h2>
-        <h1 className = 'title12'> BMI Calculator!</h1>
-          <form onSubmit={calcBmi}>
-            <div>
-              <label>Weight (lbs)</label>
-              <input value = {weight} onChange={(event) => setWeight(event.target.value)} />
-            </div>
-            <div>
-              <label>Height (in)</label>
-              <input value = {height} onChange={(event) => setHeight(event.target.value)} />
-            </div>
-            <div>
-              <button className= 'btn12' type= 'submit12'>Submit</button>
-              <button className= 'btn12 btn-outline12' onClick={reload} type= 'submit12'>Restart</button>
-            </div>
-          </form>
-          <div className='center12'>
-              <h3> Your BMI is:{bmi}</h3>
-              <p>{message}</p>
-          </div>
-          <div className = 'img-container12'>
-            <img src={imgSrc} alt =''></img>
-          </div>
-      </div>
-    </div>
+        <div className = 'home-img'>
+            <img src = {home} alt=''></img>
+        </div>
     );
 };
 
--- src/components/section2.js
+++ src/components/section2.js
@@ -1,103 +1,82 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './section2index.css'
 
 function Section2() {
+
+  const [weight, setWeight] = useState(0)
+  const [height, setHeight] = useState(0)
+  const [bmi, setBmi] = useState('')
+  const [message, setMessage] = useState('')
+
+  let imgSrc;
+
+  
+
+  let calcBmi  = (event) =>{
+    //prevetn submitting
+    event.preventDefault()
+
+    if(weight === 0 || height === 0){
+      alert('Please enter valid values')
+    }else{
+      const bmi = (weight / (height*height) *703)
+      setBmi(bmi.toFixed(1))
+
+      if(bmi<25){
+        setMessage('You are underweight')
+      }else if (bmi >= 25 && bmi < 30){
+        setMessage('You are a healthy weight')
+      }else{
+        setMessage('You are overweight')
+      }
+    }
+  }
+
+  if(bmi<1){
+    imgSrc = null
+  }else{
+    if(bmi<25){
+      imgSrc = require('../assets/skinny.jpg')
+    }else if(bmi >= 25 && bmi < 30){
+      imgSrc = require('../assets/normal.jpg')
+    }else{
+      imgSrc = require('../assets/fat.jpg')
+    }
+  } 
+
+
+  let reload = () =>{
+    window.location.reload()
+  }
+
     return (
-        <div>
-            <section className="ftco-section ftco-no-pb ftco-no-pt ftco-program bg-light" id="programs-section">
-      <div className="container">
-        <div className="row no-gutters">
-          <div className="col-md-4 ftco-animate py-5 nav-link-wrap js-fullheight">
-            <div className="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-              <a className="nav-link px-4 active" id="v-pills-1-tab" data-toggle="pill" href="#v-pills-1" role="tab" aria-controls="v-pills-1" aria-selected="true"><span className="mr-3 flaticon-gym"></span> Fitness Program</a>
-
-              <a className="nav-link px-4" id="v-pills-2-tab" data-toggle="pill" href="#v-pills-2" role="tab" aria-controls="v-pills-2" aria-selected="false"><span className="mr-3 flaticon-body"></span> Fit &amp; Healthy</a>
-
-              <a className="nav-link px-4" id="v-pills-3-tab" data-toggle="pill" href="#v-pills-3" role="tab" aria-controls="v-pills-3" aria-selected="false"><span className="mr-3 flaticon-woman"></span> Muscle Building</a>
-
-              <a className="nav-link px-4" id="v-pills-4-tab" data-toggle="pill" href="#v-pills-4" role="tab" aria-controls="v-pills-4" aria-selected="false"><span className="mr-3 flaticon-abs"></span> Bikini &amp; Body</a>
-
-              <a className="nav-link px-4" id="v-pills-5-tab" data-toggle="pill" href="#v-pills-5" role="tab" aria-controls="v-pills-5" aria-selected="false"><span className="mr-3 flaticon-running"></span> Cardio Exercise</a>
-
-              <a className="nav-link px-4" id="v-pills-6-tab" data-toggle="pill" href="#v-pills-6" role="tab" aria-controls="v-pills-6" aria-selected="false"><span className="mr-3 flaticon-meditation"></span> Power Yoga</a>
-
-              <a className="nav-link px-4" id="v-pills-7-tab" data-toggle="pill" href="#v-pills-7" role="tab" aria-controls="v-pills-7" aria-selected="false"><span className="mr-3 flaticon-aerobic"></span> Aerobics Program</a>
-
-            	<a className="nav-link px-4" id="v-pills-08-tab" data-toggle="pill" href="#v-pills-08" role="tab" aria-controls="v-pills-08" aria-selected="false"><span className="mr-3 flaticon-gym"></span> Crossfit Program</a>
+      <div className="app12">
+      <div className = 'container12'>
+        <h2 className = 'center12'BMI Calculator></h2>
+        <h1 className = 'title12'> BMI Calculator!</h1>
+          <form onSubmit={calcBmi}>
+            <div>
+              <label>Weight (lbs)</label>
+              <input value = {weight} onChange={(event) => setWeight(event.target.value)} />
             </div>
-          </div>
-          <div className="col-md-8 ftco-animate p-4 p-md-5 d-flex align-items-center js-fullheight">
-            
-            <div className="tab-content pl-md-5" id="v-pills-tabContent">
-
-              <div className="tab-pane fade show active py-0" id="v-pills-1" role="tabpanel" aria-labelledby="v-pills-1-tab">
-                <span className="icon mb-3 d-block flaticon-gym"></span>
-                <h2 className="mb-4">Fitness Program</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptate, quibusdam sunt iste dolores consequatur</p>
-                <p>Inventore fugit error iure nisi reiciendis fugiat illo pariatur quam sequi quod iusto facilis officiis nobis sit quis molestias asperiores rem, blanditiis! Commodi exercitationem vitae deserunt qui nihil ea, tempore et quam natus quaerat doloremque.</p>
-                <p><a href="./index.html" className="btn btn-primary px-4 py-3">Learn More</a></p>
-              </div>
-
-              <div className="tab-pane fade py-0" id="v-pills-2" role="tabpanel" aria-labelledby="v-pills-2-tab">
-                <span className="icon mb-3 d-block flaticon-body"></span>
-                <h2 className="mb-4">Fit &amp; Healthy</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptate, quibusdam sunt iste dolores consequatur</p>
-                <p>Inventore fugit error iure nisi reiciendis fugiat illo pariatur quam sequi quod iusto facilis officiis nobis sit quis molestias asperiores rem, blanditiis! Commodi exercitationem vitae deserunt qui nihil ea, tempore et quam natus quaerat doloremque.</p>
-                <p><a href="./index.html" className="btn btn-primary px-4 py-3">Learn More</a></p>
-              </div>
-
-              <div className="tab-pane fade py-0" id="v-pills-3" role="tabpanel" aria-labelledby="v-pills-3-tab">
-                <span className="icon mb-3 d-block flaticon-woman"></span>
-                <h2 className="mb-4">Muscle Building</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptate, quibusdam sunt iste dolores consequatur</p>
-                <p>Inventore fugit error iure nisi reiciendis fugiat illo pariatur quam sequi quod iusto facilis officiis nobis sit quis molestias asperiores rem, blanditiis! Commodi exercitationem vitae deserunt qui nihil ea, tempore et quam natus quaerat doloremque.</p>
-                <p><a href="./index.html" className="btn btn-primary px-4 py-3">Learn More</a></p>
-              </div>
-
-              <div className="tab-pane fade py-0" id="v-pills-4" role="tabpanel" aria-labelledby="v-pills-4-tab">
-                <span className="icon mb-3 d-block flaticon-abs"></span>
-                <h2 className="mb-4">Bikini &amp; Body</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptate, quibusdam sunt iste dolores consequatur</p>
-                <p>Inventore fugit error iure nisi reiciendis fugiat illo pariatur quam sequi quod iusto facilis officiis nobis sit quis molestias asperiores rem, blanditiis! Commodi exercitationem vitae deserunt qui nihil ea, tempore et quam natus quaerat doloremque.</p>
-                <p><a href="./index.html" className="btn btn-primary px-4 py-3">Learn More</a></p>
-              </div>
-
-              <div className="tab-pane fade py-0" id="v-pills-5" role="tabpanel" aria-labelledby="v-pills-5-tab">
-                <span className="icon mb-3 d-block flaticon-running"></span>
-                <h2 className="mb-4">Cardio Exercise</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptate, quibusdam sunt iste dolores consequatur</p>
-                <p>Inventore fugit error iure nisi reiciendis fugiat illo pariatur quam sequi quod iusto facilis officiis nobis sit quis molestias asperiores rem, blanditiis! Commodi exercitationem vitae deserunt qui nihil ea, tempore et quam natus quaerat doloremque.</p>
-                <p><a href="./index.html" className="btn btn-primary px-4 py-3">Learn More</a></p>
-              </div>
-
-              <div className="tab-pane fade py-0" id="v-pills-6" role="tabpanel" aria-labelledby="v-pills-6-tab">
-                <span className="icon mb-3 d-block flaticon-meditation"></span>
-                <h2 className="mb-4">Power Yoga</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptate, quibusdam sunt iste dolores consequatur</p>
-                <p>Inventore fugit error iure nisi reiciendis fugiat illo pariatur quam sequi quod iusto facilis officiis nobis sit quis molestias asperiores rem, blanditiis! Commodi exercitationem vitae deserunt qui nihil ea, tempore et quam natus quaerat doloremque.</p>
-                <p><a href="./index.html" className="btn btn-primary px-4 py-3">Learn More</a></p>
-              </div>
-
-              <div className="tab-pane fade py-0" id="v-pills-7" role="tabpanel" aria-labelledby="v-pills-7-tab">
-                <span className="icon mb-3 d-block flaticon-aerobic"></span>
-                <h2 className="mb-4">Aerobics Program</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptate, quibusdam sunt iste dolores consequatur</p>
-                <p>Inventore fugit error iure nisi reiciendis fugiat illo pariatur quam sequi quod iusto facilis officiis nobis sit quis molestias asperiores rem, blanditiis! Commodi exercitationem vitae deserunt qui nihil ea, tempore et quam natus quaerat doloremque.</p>
-                <p><a href="./index.html" className="btn btn-primary px-4 py-3">Learn More</a></p>
-              </div>
-
-              <div className="tab-pane fade py-0" id="v-pills-08" role="tabpanel" aria-labelledby="v-pills-08-tab">
-                <span className="icon mb-3 d-block flaticon-gym"></span>
-                <h2 className="mb-4">Crossfit Program</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptate, quibusdam sunt iste dolores consequatur</p>
-                <p>Inventore fugit error iure nisi reiciendis fugiat illo pariatur quam sequi quod iusto facilis officiis nobis sit quis molestias asperiores rem, blanditiis! Commodi exercitationem vitae deserunt qui nihil ea, tempore et quam natus quaerat doloremque.</p>
-                <p><a href="./index.html" className="btn btn-primary px-4 py-3">Learn More</a></p>
-              </div>
+            <div>
+              <label>Height (in)</label>
+              <input value = {height} onChange={(event) => setHeight(event.target.value)} />
             </div>
+            <div>
+              <button className= 'btn12' type= 'submit12'>Submit</button>
+              <button className= 'btn12 btn-outline12' onClick={reload} type= 'submit12'>Restart</button>
+            </div>
+          </form>
+          <div className='center12'>
+              <h3> Your BMI is:{bmi}</h3>
+              <p>{message}</p>
           </div>
-        </div>
+          <div className = 'img-container12'>
+            <img src={imgSrc} alt =''></img>
+          </div>
       </div>
-    </section>
-        </div>
+    </div>
     );
 };
 
